84f28f50a5fb1735ef555462f11345710d98c6cc
Ben <2485840025@qq.com>
2025-07-12 23:25:52 +0800

    更新设备选择配置，修改时间戳和设备标识符，优化主界面布局，提升用户交互体验和界面美观性。

--- app/src/main/java/com/citadawn/speechapp/MainActivity.java
+++ app/src/main/java/com/citadawn/speechapp/MainActivity.java
@@ -14,13 +14,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
@@ -272,6 +271,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         tts = new TextToSpeech(this, status -> {
+
             if (status == TextToSpeech.SUCCESS) {
                 int result = tts.setLanguage(currentLocale);
                 tts.setSpeechRate(speechRate);
